e3d04ffaafe4a1dc497e9563385b1454ad28ba33
thanglong0802 <thanglong08022k@gmail.com>
2022-07-07 17:58:58 +0700

    signup and active link

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -10,39 +10,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-  
-  message='';
-  formEmail=new FormGroup({
-    email:new FormControl('',[ Validators.email,Validators.required]),
+
+  message = '';
+  formEmail = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
   });
 
-  constructor(  private forgotPasswordService: ForgotPasswordService,private fb: FormBuilder,
-    private router: Router,) { }
+  constructor(private forgotPasswordService: ForgotPasswordService, private fb: FormBuilder,
+              private router: Router,) {
+  }
 
   ngOnInit(): void {
     this.initForm();
   }
+
   initForm() {
     this.formEmail = this.fb.group({
-      email: ['',[Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
-  
-  onSubmit(){
-    if ( this.formEmail.valid) {
+
+  onSubmit() {
+    if (this.formEmail.valid) {
       const email = this.formEmail.controls.email.value;
       this.forgotPasswordService.tranferMail(email);
       this.forgotPasswordService.sendOTP(email).subscribe(
-        data => { 
-          this.message=data.message
-          if(this.message == 'success')
-            this.router.navigate(['/change-password/'])
-      this.forgotPasswordService.sendOTP(this.formEmail.controls.email.value).subscribe(
         data => {
-              this.message=data.message   
+          this.message = data.message
+          if (this.message == 'success')
+            this.router.navigate(['/change-password/'])
+          this.forgotPasswordService.sendOTP(this.formEmail.controls.email.value).subscribe(
+            data => {
+              this.message = data.message
               console.log(this.message);
-        }
-      );
+            }
+          );
+        });
     }
   }
 }
